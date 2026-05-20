853c0880ac73b9c6387831e42df592dc03771ead
WTomas <vajdatomi98@gmail.com>
2026-05-20 01:53:01 +0100

    release: v0.2.1

--- wrappers/pip/src/sufleur_cli/__init__.py
+++ wrappers/pip/src/sufleur_cli/__init__.py
@@ -2,6 +2,6 @@ from __future__ import annotations
 
 from ._find_sufleur import SufleurNotFound, find_sufleur_bin
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = ["SufleurNotFound", "find_sufleur_bin"]
